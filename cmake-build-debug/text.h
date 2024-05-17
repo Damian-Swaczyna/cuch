a2a9d495bd59e23dec65947ea89186b8a55df2b1
Damian Swaczyna <damianswa12@gmail.com>
2024-05-17 02:01:23 +0200

    123

---
+++ cmake-build-debug/text.h
@@ -0,0 +1,13 @@
+//
+// Created by Admin on 30.04.2024.
+//
+
+#ifndef untitled6_TEXT_H
+#define untitled6_TEXT_H
+void entertext(char *text);
+void printtext(char *text);
+void clear(void);
+int count(char *text);
+void bigLetters(char *ciag);
+void smallLetters(char *ciag);
+#endif //untitled6_TEXT_H
